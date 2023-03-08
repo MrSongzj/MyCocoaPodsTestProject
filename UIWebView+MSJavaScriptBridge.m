b0917490bfd02148b1134e40badc19b66278bc3b
mrsong <424607870@qq.com>
2023-03-08 11:19:00 +0800

    update

--- UIWebView+MSJavaScriptBridge.m
+++ UIWebView+MSJavaScriptBridge.m
@@ -180,7 +180,7 @@ NSString *wholeNameOfCallback(NSString *callbackName)
 
 - (void)ms_removeHandler3:(NSString *)handlerName
 {
-    NSLog(@"update_2");
+    NSLog(@"update_2_1");
 }
 
 #pragma mark - Notification
